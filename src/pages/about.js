306e3ff879928e2fc8b08cfe5adc3898ea55ce8e
Greg McKelvey <mckelveygreg@gmail.com>
2018-11-20 16:35:12 -0800

    updated with description

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,6 +20,10 @@ class About extends Component {
     return (
       <Layout>
         <h2>About Us...</h2>
+
+        <h3>Open Data :: Open Software :: Open Hardware :: Open Projects</h3>
+    <p>Participants can learn from Mozilla Open Leaders the basics of running open projects. Various local projects from teams at the University of Oregon (Open Data), Downtown Eugene artists (Open Hardware), Local Software Companies (Open Source Software) will be “Working Open” through the weekend in hands-on workshops. Bring your own projects and we’ll help you pick the right open source or creative commons license to help share your work with the world. Bring your energy and we’ll help you collaborate.</p>
+    <hr />
         <h3 style={{textAlign: 'center'}}>Leadership Team</h3>
         
         <div style={{display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap'}}>{leaderCards}</div>
