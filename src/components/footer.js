9459f3a1c43ba9e96dd41d925a9ecafe05a47670
Greg McKelvey <mckelveygreg@gmail.com>
2018-11-27 15:00:48 -0800

    footerUpdate, fontAwesome icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faGithub,
+  faTwitter,
+  faFacebook,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () => (
   <div
@@ -7,8 +14,60 @@ const Footer = () => (
       margin: '0',
     }}
   >
-    <p style={{margin: 'auto', textAlign: 'center', padding: 15}}>social media icons, etc</p>
+    <div
+      className="socialMediaIcons"
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+      }}
+    >
+      <a
+        href="https://github.com/EugTech"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faGithub} />
+      </a>
+      <a
+        href="https://twitter.com/EugTech"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faTwitter} />
+      </a>
+      <a
+        href="https://www.facebook.com/EugTech/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faFacebook} />
+      </a>
+      <a
+        href="https://www.instagram.com/eugtech/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faInstagram} />
+      </a>
+
+      <img
+        src="https://siliconshire.org/static/images/stamp_made_white.png"
+        alt="Made in the shire logo"
+        style={{ margin: 0 }}
+      />
+    </div>
+    <div style={{textAlign: 'center', paddingBottom: '1rem', background: '#68676788'}}>
+      <a style={{color: 'white'}}
+        href="https://www.facebook.com/events/196679251226823/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Check out the facebook event!
+      </a>
+    </div>
   </div>
 )
 
-export default Footer;
+export default Footer
