b9245580cbb8a2b2ff8419e57880eac7edaf0e82
Mark Davis <GitKraken@nohorse.com>
2018-11-27 09:34:55 -0800

    fresh content

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -21,16 +21,41 @@ class ProjectsPage extends Component {
       <Layout>
         <h2>Eugene Open Source Projects</h2>
         <hr />
-        <h3>Submit a project</h3>
+        <h3>Bring you Project, We'll help you open it!</h3>
         <p>
-          We are looking for projects that collaborators can work on during the
-          event. Maybe you have a project that is up and running but not
-          open-sourced. We can help take you to the next level. Perhaps you have
-          an idea for a project and would like to start it out by working in the
-          open. We can get you started. Or maybe you have some data stuck in a
-          flat file somewhere you’d like to see made available to the world or
-          have a PI built. That works too! We’d love to build community
-          connections between projects and contributors.
+        Bring your project and we'll help you open it!
+No matter the current state of your project, we can make it more open.  What does “open” mean?
+<ol>
+  <li>Easy to understand,</li>
+  <li>Easy to share, remix and build on, and,</li>
+  <li>Welcoming to all participants, from any background.</li>
+</ol>
+
+This applies to software projects, hardware projects, art projects, events and anything that you would like to work on with others locally and online.  Here’s some examples
+
+<table>
+<tr>
+    <th>Current State of your project</th>
+    <th>Possible state after the event</th>
+  </tr>
+  <tr>
+    <td>I have a great idea!</td>
+    <td>You have launched a project online with all the things folks need to collaborate with you!</td>
+  </tr>
+  <tr>
+    <td>I have an open project on github but nobody is contributing.</td>
+    <td>Now your project is welcoming and inviting to folks and you have issues for folks to work on.</td>
+  </tr>
+  <tr>
+    <td>I have a project I’d like to share, but I’d like to keep my branding private for reasons.</td>
+    <td>Now the guts of your project is open, but your branding is protected by a licenses that preserves control over your brand.</td>
+  </tr>
+  <tr>
+    <td>I made a giant pong controller and now folks in Chattanooga want one, but I don’t want to build it or ship it to them</td>
+    <td>You've publish the plan on Instructables, add a creative commons license and they can build it themselves!</td>
+  </tr>
+</table>
+
         </p>
         <h3 style={{textAlign: "center"}}>
           Fill in the <a href="https://goo.gl/forms/xQsoEPznuxRDdhNz2" target="_blank" rel="noopener noreferrer">project form here</a>
