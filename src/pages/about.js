2dd9f7935c85e1d5d95441404cf246078a4eccb8
Mark Davis <GitKraken@nohorse.com>
2018-11-30 20:37:11 -0800

    minor edit

--- src/pages/about.js
+++ src/pages/about.js
@@ -22,7 +22,7 @@ class About extends Component {
         <h2 style={{textAlign: 'center'}}>About Us...</h2>
 
         <h3 style={{textAlign: 'center'}}>Open Data :: Open Software :: Open Hardware :: Working Open</h3>
-    <p>Participants can learn from local Open Leaders the basics of running open projects. W'ell be “Working Open” through the weekend in hands-on workshops. Bring your energy and we’ll help you collaborate.</p>
+    <p>Participants can learn from local Open Leaders the basics of running open projects. We'll be “Working Open” through the weekend in hands-on workshops. Bring your energy and we’ll help you collaborate.</p>
     <p>This all started with a "Call for Leaders" on the EugeneTech Switcboard. The folks below stepped up.</p>
     <hr />
         <h3 style={{textAlign: 'center'}}>Leadership Team</h3>
