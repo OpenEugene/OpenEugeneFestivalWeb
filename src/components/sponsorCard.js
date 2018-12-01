03f195b1c3d42419d6430276a29dbfdffc0e5ad8
Greg McKelvey <mckelveygreg@gmail.com>
2018-11-30 17:52:07 -0800

    logos update

--- src/components/sponsorCard.js
+++ src/components/sponsorCard.js
@@ -1,15 +1,15 @@
 import React from "react";
 
 const SponsorCard = props => {
-  // console.log(props.LogoURL)
+  //console.log(props.LogoURL)
   let logoSrc
   if (props.LogoURL !== '' ) {
-    logoSrc = require('../images/SponsorLogos' + props.LogoURL)
+    logoSrc = require(`../images/SponsorLogos/` + props.LogoURL)
   }
-  // console.log(logoSrc)
+  console.log(logoSrc)
   return (
     <div>
-          <a href={props.Link} >
+          <a href={props.Link} style={{textAlign: "center"}}>
         <h4 className={props.Level}>{props.Name}</h4>
             {props.LogoURL !== "" ? (<img src={logoSrc} alt={props.Name}/>) : ''}
           </a>
--- src/pages/sponsors.js
+++ src/pages/sponsors.js
@@ -14,13 +14,12 @@ class Sponsors extends Component {
         <h2>Open Eugene Sponsors</h2>
         <h3>
           <a href="https://eugtech.github.io/Open-Eugene/open-eugene-fest-sponsorship.pdf">
-            Become a sponsor!
           </a>
         </h3>
         <hr />
         <h3>Title Sponsor</h3>
+          {sponsorCards.filter(card => card.props.Level === 'Title')}
         {/* {sponsorCards.filter(card => card.props.Level === 'Title')} */}
-        Your logo here!
         <hr />
         <h3>Presenting Sponsors</h3>
         <div className="sponsorContainer">
