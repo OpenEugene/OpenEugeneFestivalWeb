65cce3ada1ae87fade060b5230a1483aaed3be3a
Greg McKelvey <mckelveygreg@gmail.com>
2018-12-11 06:22:48 -0800

    find out more projects link

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React from 'react'
 
 const ProjectCard = props => {
   return (
     <div>
-        <h3 className="projectName">{props["Project Name"]}</h3>
-        <h4>Created by: {props["Your Name"]}</h4>
-        <ul>
-          <li>Type of Project: {props["Type of Project"]}</li>
-          <li>Stage of Project: {props["What stage is your project in today?"]}</li>
-          <li>Description: {props["Longer Description"]}</li>
-        </ul>
-        <hr></hr>
+      <h3 className="projectName">{props['Project Name']}</h3>
+      <h4>Created by: {props['Your Name']}</h4>
+      <ul>
+        <li>Type of Project: {props['Type of Project']}</li>
+        <li>
+          Stage of Project: {props['What stage is your project in today?']}
+        </li>
+        <li>Description: {props['Longer Description']}</li>
+        {props['Link to GitHub or other Repo, Google docs, etc.'] === '' ? (
+          ''
+        ) : (
+          <li>
+            <a href={props['Link to GitHub or other Repo, Google docs, etc.']}>
+              Find out more!
+            </a>
+          </li>
+        )}
+      </ul>
+      <hr />
     </div>
-  );
-};
+  )
+}
 
-export default ProjectCard;
+export default ProjectCard
