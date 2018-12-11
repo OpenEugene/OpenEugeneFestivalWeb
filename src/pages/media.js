e49013f461b964cc714114db2240a8d7d71230ec
Greg McKelvey <mckelveygreg@gmail.com>
2018-12-10 22:50:39 -0800

    media cards rough draft

--- src/pages/media.js
+++ src/pages/media.js
@@ -1,45 +1,40 @@
-import React, { Component } from 'react';
-import Layout from '../components/layout';
-
-const mediaSheet = 'https://script.googleusercontent.com/macros/echo?user_content_key=93-ltdAnPMem2Q32hAc8nX6jANdP3QgZ3EOOzQrD8RGZnO5hYtrVr5TGf6JfV9TGD1bW7yVLiO5nsT9aMQMsCzYiqjPUFsYzm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnAqgXoWBTgCVBnNERKTjEvDsGfBsZoiU7LHd4mCAnYyko_pPCyXCr_yuiPLIegSwjw&lib=MwltHTXjXpuiGDLxc5pNpa7W3JIQpNf-7'
+import React, { Component } from 'react'
+import Layout from '../components/layout'
+import mediaJSON from '../json/media'
+import ArticleCard from '../components/articleCard'
+import VideoCard from '../components/videoCard'
+import AudioCard from '../components/audioCard'
 
 class Media extends Component {
-  state = {
-    articles: [],
-    videos: [],
-    audio: [],
-  }
-
-  async componentDidMount() {
-    const mediaData = await fetch(mediaSheet).then(result => result.json()).then(data => {
-      return (
-        this.setState({
-          articles: data.filter(item => item.type === "article"),
-          videos: data.filter(item => item.type === "video"),
-          audio: data.filter(item => item.type === "audio"),
-        })
-      )
-    });
-    console.log(mediaData);
-  }
   render() {
+    const articleCards = mediaJSON
+      .filter(item => item.type === 'article')
+      .map((item, i) => <ArticleCard key={i} {...item} />)
+
+    const videoCards = mediaJSON
+      .filter(item => item.type === 'video')
+      .map((item, i) => <VideoCard key={i} {...item} />)
+
+    const audioCards = mediaJSON
+      .filter(item => item.type === 'audio')
+      .map((item, i) => <AudioCard key={i} {...item} />)
+
     return (
       <Layout>
         <h2>Check out some media surrounding Open Eugene!</h2>
         <hr />
         <h3>Articles</h3>
-        {this.state.articles}
+        {articleCards}
         <hr />
         <h3>Videos</h3>
-        {this.state.videos}
+        {videoCards}
         <hr />
         <h3>Audio</h3>
-        {this.state.audio}
+        {audioCards}
         <hr />
       </Layout>
     )
   }
-
 }
 
-export default Media;
+export default Media
