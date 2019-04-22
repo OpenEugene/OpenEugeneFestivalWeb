ca99597d9962f7a76d179aa16cc15dac2cd1a0c7
Mark Davis <GitKraken@nohorse.com>
2019-04-22 10:53:41 -0700

    added sprint infos

--- src/pages/sprint2019.js
+++ src/pages/sprint2019.js
@@ -17,9 +17,14 @@ class Sprint2019 extends Component {
     return (
       <Layout>
         <h2 style={{textAlign: 'center'}}>Sprint 2019</h2>
-
-    <p>Part of the <a href="https://foundation.mozilla.org/en/opportunity/open-events-guide/about-the-2019-sprint-for-internet-health/">Mozilla Global Sprint for Internet Health</a> the following projects are participating in the</p>
+   
+    <p>Part of the <a href="https://foundation.mozilla.org/en/opportunity/open-events-guide/about-the-2019-sprint-for-internet-health/">Mozilla Global Sprint for Internet Health</a></p>
     <hr />
+
+    <h3 style={{textAlign: 'center'}}>Two events in May 2019</h3>
+    <h4 style={{textAlign: 'center'}}>Kickoff <a href="https://www.meetup.com/EugeneTech/events/260713803"> May 1st 5PM at CodeChops</a></h4>
+    <h4 style={{textAlign: 'center'}}>Wrap-up <a href="https://www.meetup.com/EugeneTech/events/260714202"> May 29th 5PM at CodeChops</a></h4>
+    
         <h3 style={{textAlign: 'center'}}>Purpose</h3>
         <p>To help local open source projects in Eugene, OR gain visibility and traction from the greater open source community.</p>
 
@@ -32,6 +37,8 @@ class Sprint2019 extends Component {
         <li>Repo given topics of "open-eugene" and "global-sprint" to participate in the</li>
         <li>Project maintainers bring stories to the finishing event and share.</li>
         </ol>
+
+        <h3 style={{textAlign: 'center'}}>The following projects are participating</h3>
         
       </Layout>
     )
